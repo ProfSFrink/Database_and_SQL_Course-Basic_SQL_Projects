9213d74346846a89448ca45c3f801ae4a1e8d6cc
Steven Partlow <97795561+ProfSFrink@users.noreply.github.com>
2022-10-17 11:42:24 +0100

    Complete 'Stored Procedure Assignment'

---
+++ Stored Procedure Assignment.sql
@@ -0,0 +1,20 @@
+-- Advanced Software and Web Developer Diploma
+-- Course #7 - Database and SQL
+-- Database and SQL Basic Tutorial Video Series
+
+--
+-- STORED PROCEDURE ASSIGNMENT (17/10/22)
+
+/* Use the database 'AdventureWorks' */
+USE AdventureWorks
+GO
+
+/* Create a new stored procedure 'dbo.uspGetAddress' */
+CREATE PROCEDURE dbo.uspGetAddress
+AS
+SELECT * FROM Person.Address -- SELECT all rows from the 'Person.Address' column
+GO
+
+EXEC dbo.uspGetAddress -- EXECUTE the store procedure 'dbo.uspGetAddress'
+
+DROP PROCEDURE IF EXISTS dbo.uspGetAddress -- DROP the stored procedure 'dbo.uspGetAddress' IF it exists in the database

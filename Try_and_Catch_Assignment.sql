f17093be07a5040fb80eda4cbbe77d0937febb52
Steven Partlow <97795561+ProfSFrink@users.noreply.github.com>
2022-10-18 12:12:04 +0100

    Create Try_and_Catch_Assignment.sql
    Complete 'Try / Catch Assignment'

---
+++ Try_and_Catch_Assignment.sql
@@ -0,0 +1,24 @@
+-- Advanced Software and Web Developer Diploma
+-- Course #7 - Database and SQL
+-- Database and SQL Basic Tutorial Video Series
+
+--
+-- TRY / CATCH ASSIGNMENT (18/10/22)
+
+/* Use the 'AdventureWorks' database */
+USE AdventureWorks
+GO
+
+/* TRY this */
+BEGIN TRY
+	/* Attempt to divide 1000 by 0 and store the result in variable 'Result' */
+	SELECT 1000/0 AS Result;
+END TRY /* End TRY */
+
+/* If the above TRY block raises an exception then */
+BEGIN CATCH
+	SELECT
+		ERROR_NUMBER() AS [Error_Code], -- Store the error code in variable 'Error_Code'
+		ERROR_PROCEDURE() AS [Invalid_Proc], -- Store the error procedure in variable 'Invalid_Proc'
+		ERROR_MESSAGE() AS [Error_Details]; -- -- Store the error message in variable 'Error_Details'
+END CATCH

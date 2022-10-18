ded3275f08c9e37f76edbaf4e19f662b83fa784f
Steven Partlow <97795561+ProfSFrink@users.noreply.github.com>
2022-10-18 14:54:56 +0100

    Complete 'Stored Procedure Assignment 2'

---
+++ Stored Procedure Assignment 2.sql
@@ -0,0 +1,18 @@
+-- Advanced Software and Web Developer Diploma
+-- Course #7 - Database and SQL
+-- Database and SQL Basic Tutorial Video Series
+
+--
+-- STORED PROCEDURE ASSIGNMENT #2 (18/10/22)
+
+/* Use the 'AdventureWorks' database */
+USE AdventureWorks
+GO
+
+/* Create a stored procedure called 'uspGetAddress2' and give control of it to the database owner */
+CREATE PROCEDURE dbo.uspGetAddress2 @City NVARCHAR(30)
+AS
+SELECT * FROM Person.Address /* SELECT all rows in the address column */
+WHERE City LIKE @City + '%' /* WHERE the 'City' column contains the value of the variable '@City' in addition to any other characters*/
+GO
+EXEC dbo.uspGetAddress2 @City = 'New' /* We EXECute the stored procedure 'dbo.uspGetAddress2' and pass in the arguments 'New' */
